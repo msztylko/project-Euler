f1dc13a19ab10a1191bb4fdfe072a9de744fe8e0
Marcin <marcin.sztylko@gmail.com>
2019-07-19 16:13:33 +0200

    Solution to the 13th problem

---
+++ problem_13/p013.py
@@ -0,0 +1,10 @@
+with open("problem_13.txt", "r") as f:
+    lines = f.readlines()
+    sum = 0
+    for line in lines:
+        if line[0].isdigit():
+            sum += int(line)
+
+    print("The 10 first digits of this sum are", str(sum)[:10])
+
+#CORRECT
