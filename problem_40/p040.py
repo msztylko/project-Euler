9129be382f51a2eed209ff90523919937931d312
Marcin <marcin.sztylko@gmail.com>
2019-08-07 18:26:31 +0200

    Solution to the 40th problem

---
+++ problem_40/p040.py
@@ -0,0 +1,15 @@
+import sys
+if sys.version_info.major == 2:
+	range = xrange
+
+
+def compute():
+	s = "".join(str(i) for i in range(1, 1000000))
+	ans = 1
+	for i in range(7):
+		ans *= int(s[10**i - 1])
+	return str(ans)
+
+
+if __name__ == "__main__":
+	print(compute())
