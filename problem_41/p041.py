a470163f9703cb06bd574a96a4f70cf3c7b6a20f
Marcin <marcin.sztylko@gmail.com>
2019-08-09 18:35:34 +0200

    Final solution to the 41st problem

--- problem_41/p041.py
+++ problem_41/p041.py
@@ -1,18 +1,25 @@
-def is_pandigital(n):
-
+def isPandigital(n):
+    compare = []
+    for i in range(1, len(str(n)) + 1):
+        compare.append(str(i))
+    if sorted(str(n)) == compare:
+        return True
 
 def isPrime(n):
-    if n == 2 or n == 3: return True
-    if n % 2 == 0 or n < 2: return False
-    for i in range(3, int(n**0.5) + 1, 2):   # only odd numbers
+    if n < 2:
+        return False
+    i = 2
+    while (i * i <= n):
         if n % i == 0:
             return False
+        i = i + 1
+    return True
 
 
 def main():
     maxnum = 0
-    for i in range(3, 10000000000, 2):
-        if isPrime(n) and isPandigital(n) and n > maxnum:
+    for i in range(3, 7654321, 2): #7654321 is the largest possible solution, 8- and 9-digit pandigital numbers are divisible by 3
+        if isPrime(i) and isPandigital(i) and i > maxnum:
             maxnum = i
 
     return maxnum
