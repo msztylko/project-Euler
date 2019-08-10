aefcedd4ab8515f2c484b6d550ddf30a2255f2f1
Marcin <marcin.sztylko@gmail.com>
2019-08-10 14:13:00 +0200

    code moved to solution function

--- problem_1/p001.py
+++ problem_1/p001.py
@@ -1,5 +1,6 @@
-sum = 0
-for i in range(1, 1000):
-    if i % 3 == 0 or i % 5 == 0: sum +=i
+def solution():
+    return sum(i for i in range(1000) if (i % 3 == 0 or i % 5 == 0))
 
-print(sum)
+
+if __name__ == "__main__":
+    print("The sum of all the multiples of 3 or 5 below 1000 is equal to {}".format(solution()))
