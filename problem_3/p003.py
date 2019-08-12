36abe8e55daff7093cbcde09157f3e4b78e8840c
Marcin <marcin.sztylko@gmail.com>
2019-08-12 15:29:27 +0200

    code moved to solution function

--- problem_3/p003.py
+++ problem_3/p003.py
@@ -1,8 +1,12 @@
-n = 600851475143
-i = 2
-while i * i < n:
-    while n % i == 0:
-        n = n / i
-    i = i + 1
+def largest_prime_factor(n):
+    i = 2
+    while i**2 < n:
+        while n % i == 0:
+            n /= i
+        i += 1
 
-print(n, i-1)
+    return int(n)
+
+
+if __name__ == "__main__":
+    print("The largest prime factor of the number 600851475143 is {}".format(largest_prime_factor(600851475143)))
