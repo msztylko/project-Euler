af9501242d7a82148fd9d38edf8168b85d9c4ed1
Marcin <marcin.sztylko@gmail.com>
2019-08-12 16:06:21 +0200

    code moved to solution function

--- problem_6/p006.py
+++ problem_6/p006.py
@@ -1,8 +1,6 @@
-sum1 = 0
-sum2 = 0
-for i in range(1, 101):
-    sum1 = sum1 + i**2
-    sum2 = sum2 + i
-square = sum2**2
-diff = square - sum1
-print(diff)
+def solution():
+    return sum(x for x in range(1, 101))**2 - sum(x**2 for x in range(1, 101))
+
+if __name__ == "__main__":
+    print("The difference between the sum of the squares of the first one hundred natural "
+          "numbers and the square of the sum is equal {}".format(solution()))
