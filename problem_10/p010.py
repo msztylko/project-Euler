fa0d270f1ae158254eeaecc51515e503b170d929
Marcin <marcin.sztylko@gmail.com>
2019-08-13 22:54:38 +0200

    code moved to solution function and use of list comprehension

--- problem_10/p010.py
+++ problem_10/p010.py
@@ -2,15 +2,16 @@ def is_prime(n):
     if n < 2:
         return False
     i = 2
-    while (i * i <= n):
+    while i * i <= n:
         if n % i == 0:
             return False
         i = i + 1
     return True
 
-primes = []
-for i in range(2,2000001):
-    if is_prime(i) == True:
-        primes.append(i)
 
-print(sum(primes))
+def solution():
+    return sum(i for i in range(2, 2000001) if is_prime(i))
+
+
+if __name__ == "__main__":
+    print("The sum of all the primes below two million is equal {}".format(solution()))
