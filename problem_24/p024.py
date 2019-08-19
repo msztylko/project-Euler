33a61956b23b61915a83567fcfe608eaa1f608ec
Marcin <marcin.sztylko@gmail.com>
2019-08-19 23:55:38 +0200

    code moved to solution function

--- problem_24/p024.py
+++ problem_24/p024.py
@@ -1,7 +1,12 @@
 import itertools
 
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-permutations = list(itertools.permutations(numbers))
-for (i, perm) in enumerate(permutations):
-    if i == 999999:
-        print('The the millionth lexicographic permutation of the digits {0} is'.format(numbers), perm)
+def solution():
+    permutations = list(itertools.permutations(numbers))
+    for (i, perm) in enumerate(permutations):
+        if i == 999999:
+            return perm
+
+
+if __name__ == "__main__":
+    print('The the millionth lexicographic permutation of the digits {0} is {1}'.format(numbers, solution()))
