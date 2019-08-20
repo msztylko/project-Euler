5a326ee3e36488b8c9f07dda8ec9738d60348c79
Marcin <marcin.sztylko@gmail.com>
2019-08-20 23:24:21 +0200

    Code moved to solution function

--- problem_30/p030.py
+++ problem_30/p030.py
@@ -1,10 +1,15 @@
-sum = 0
-for number in range(2, 1000001):
-    number = str(number)
-    digit_sum = 0
-    for digit in number:
-        digit_sum += int(digit)**5
-    if digit_sum == int(number):
-        sum += int(number)
+def solution():
+    sum = 0
+    for number in range(2, 1000001):
+        number = str(number)
+        digit_sum = 0
+        for digit in number:
+            digit_sum += int(digit)**5
+        if digit_sum == int(number):
+            sum += int(number)
 
-print(sum)
+    return sum
+
+if __name__ == "__main__":
+    print("The sum of all the numbers that can be written "
+          "as the sum of fifth powers of their digits is equal {}".format(solution()))
