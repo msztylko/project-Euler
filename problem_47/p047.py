cfcc49ab23b24d634dbe4a1e8bd375a9754faab2
Marcin <marcin.sztylko@gmail.com>
2019-09-28 18:50:14 +0200

    final

--- problem_47/p047.py
+++ problem_47/p047.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def number_prime_factors(n):
     i = 2
     factors = set()
@@ -28,29 +27,3 @@ while True:
                     print(j-3)
                     break
     j += 1
-||||||| merged common ancestors
-=======
-import math
-
-def is_prime(n):
-    for i in range(2, n // 2 + 1):
-        if n % i == 0:
-            return False
-
-    return True
-
-
-def prime_factors(n):
-    prime_factors = []
-    for i in range(1, int(math.sqrt(n) + 1)):
-        if n % i == 0 and is_prime(i):
-            prime_factors.append(i)
-
-    return prime_factors
-
-
-def solution():
-    pass
-
-print(prime_factors(98765))
->>>>>>> ff7a7c1e3fca62c27a8c6920bdf761cfa92139ce
