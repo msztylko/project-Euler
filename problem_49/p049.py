e8599bd1785f73cc8773603b5e65f2c28854cdd0
Marcin <marcin.sztylko@gmail.com>
2019-09-29 23:54:20 +0200

    add is_prime function

--- problem_49/p049.py
+++ problem_49/p049.py
@@ -6,28 +6,4 @@ def is_prime(n):
     return True
 
 
-prime_list = []
-for i in range(1000, 9999):
-    if is_prime(i):
-        prime_list.append(i)
-
-
-permutations = []
-for i in prime_list:
-    for j in prime_list:
-        if sorted(str(i)) == sorted(str(j)):
-            permutations.append(i)
-
-
-solution = []
-for i in permutations:
-    for j in permutations:
-        for k in permutations:
-            if abs(i - j) == abs(j - k) == abs(k - i):
-                solution.append(i)
-
-    if len(solution) % 2 == 0:
-        print(solution)
-
-
 
