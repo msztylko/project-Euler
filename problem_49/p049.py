cdd83360b5372f8960101f0a8cab61c5970586e4
Marcin <marcin.sztylko@gmail.com>
2019-09-30 16:34:42 +0200

    final solution

--- problem_49/p049.py
+++ problem_49/p049.py
@@ -6,4 +6,29 @@ def is_prime(n):
     return True
 
 
+def is_perm(n, m):
+    if sorted(str(n)) == sorted(str(m)):
+        return True
+
+
+primes =[]
+for number in range(1000, 10000):
+    if is_prime(number):
+        primes.append(number)
+
+for i in primes:
+    for j in primes:
+        if i == j:
+            continue
+        elif i < j:
+            k = j + (j - i)
+            if k < 10000 and is_prime(k):
+                if is_perm(i, j) and is_perm(i, k):
+                    solution = str(i) + str(j) + str(k)
+
+print(solution)
+
+
+
+
 
