b568e2b569bd62e8aae358b315fe4438258df2bd
Marcin <marcin.sztylko@gmail.com>
2019-10-16 12:33:31 +0200

    optimized solution

--- problem_12/p012.py
+++ problem_12/p012.py
@@ -1,23 +1,10 @@
-import cProfile
 import math
 
-#Initial code was super slow, so I used C Profiler to find out that the problems were caused by num_divisors function.
-#The function was implemented with new logic and works much faster now.
-
 def triangle_number(n):
-    return sum(i for i in range(1, n + 1))
+    return int(n*(n+1)/2)
 
-# #Initial num_divisors function
-# def num_divisors(n):
-#     counter = 0
-#     for i in range(1, n + 1):
-#         if n % i == 0:
-#             counter += 1
-#
-#     return counter
-# #    return len([i for i in range(1, n + 1) if n % i == 0]) #A bit slower than the version below
 
-def num_divisors(n):
+def count_divisors(n):
     divs = [1]
     for i in range(2, int(math.sqrt(n)) + 1):
         if n % i == 0:
@@ -28,11 +15,12 @@ def num_divisors(n):
 
 def solution():
     i = 1
-    while num_divisors(triangle_number(i)) < 500:
+    while count_divisors(triangle_number(i)) < 500:
         i += 1
     return triangle_number(i)
 
 
 if __name__ == "__main__":
-    print("The value of the first triangle number to have over five hundred divisors is {}".format(solution()))
-    cProfile.run('solution()', sort='cumtime')
+#   import cProfile
+   print("The value of the first triangle number to have over five hundred divisors is {}".format(solution()))
+#   cProfile.run('solution()', sort='cumtime')
