2c1eaa93b1c258e3b89be26df49348cbbc94618a
Marcin <marcin.sztylko@gmail.com>
2020-01-16 07:12:22 +0100

    brute-force in progress

--- problem_60/p060.py
+++ problem_60/p060.py
@@ -1,3 +1,6 @@
+#TO DO
+#brute-force solution but it is needs some work
+
 def is_prime(n):
     if n <= 1:
         return False
@@ -9,22 +12,45 @@ def is_prime(n):
     return True
 
 
-def prime_list():
-    prime_list = []
-    for i in range(1_000_000):
-        if is_prime(i):
-            prime_list.append(i)
+# def prime_list():
+#     prime_list = []
+#     for i in range(1_000_000):
+#         if is_prime(i):
+#             prime_list.append(i)
+#
+#     return prime_list
 
-    return prime_list
+prime_list = []
+for i in range(1_000):
+    if is_prime(i):
+        prime_list.append(i)
 
 
 def concat_prime(a, b):
     n1 = int(str(a) + str(b))
     n2 = int(str(b) + str(a))
-    if is_prime(n1) and is_prime(n2):
-        return True
+    if n1 == n2:
+        if is_prime(n1):
+            return True
+        else:
+            return False
+    else:
+        if is_prime(n1) and is_prime(n2):
+            return True
+        else:
+            return False
 
-print(concat_prime(3,))
 
 def main():
-    pass
+    for i in range(len(prime_list)):
+        for j in range(i, len(prime_list)):
+            if concat_prime(prime_list[i], prime_list[j]):
+                for k in range(j, len(prime_list)):
+                    if concat_prime(prime_list[j], prime_list[k]):
+                        for l in range(k, len(prime_list)):
+                            if concat_prime(prime_list[k], prime_list[l]):
+                                return prime_list[l]
+
+
+if __name__ == "__main__":
+    print(main())
